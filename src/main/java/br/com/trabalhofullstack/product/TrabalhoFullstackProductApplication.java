715b3e17e1edb7dac6c05077452da8cc5f29b498
Gustavo <gustavo.antunes.rocha@outlook.com>
2022-08-29 10:51:47 -0300

    Start project

---
+++ src/main/java/br/com/trabalhofullstack/product/TrabalhoFullstackProductApplication.java
@@ -0,0 +1,13 @@
+package br.com.trabalhofullstack.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFullstackProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFullstackProductApplication.class, args);
+	}
+
+}
