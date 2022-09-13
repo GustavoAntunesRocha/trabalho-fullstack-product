bfaa7747a58692934c2e20ed57bd4adc47d00e0c
Gustavo <gustavo.antunes.rocha@outlook.com>
2022-09-13 08:11:21 -0300

    Add cross origin config

--- src/main/java/br/com/trabalhofullstack/product/controller/CategoryController.java
+++ src/main/java/br/com/trabalhofullstack/product/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import br.com.trabalhofullstack.product.domain.CategoryDto;
 import br.com.trabalhofullstack.product.service.CategoryService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/category")
 public class CategoryController {
 
--- src/main/java/br/com/trabalhofullstack/product/controller/ProductController.java
+++ src/main/java/br/com/trabalhofullstack/product/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import br.com.trabalhofullstack.product.domain.ProductDto;
 import br.com.trabalhofullstack.product.service.ProductService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/product")
 public class ProductController {
 
